f11a32583736a22b1a54521ac9de4d222276a81c
michaelvargas7 <michael.c.v.m.dev@gmail.com>
2026-08-17 06:56:55 -0500

    fix(search): importar AuthModule para resolver JwtService en JwtGuard

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { SearchController } from './infrastructure/http/search.controller';
 
 @Module({
+  imports: [AuthModule],
   controllers: [SearchController],
 })
 export class SearchModule {}
